dc66abd1d94b931f70e79cbee50c29ce26e1db26
Nindya Hapsari <nindyahapsariiljas@yahoo.com>
2019-02-05 11:18:50 +0100

    First commit of assignment project

---
+++ MOOCTextEditor/src/spelling/DictionaryBST.java
@@ -0,0 +1,45 @@
+package spelling;
+
+import java.util.Objects;
+import java.util.TreeSet;
+
+
+/**
+ * @author UC San Diego Intermediate MOOC team
+ *
+ */
+public class DictionaryBST implements Dictionary 
+{
+   private TreeSet<String> dict;
+   
+   
+   public DictionaryBST(){
+	   dict = new TreeSet<>();
+   }
+	
+    
+    /** Add this word to the dictionary.  Convert it to lowercase first
+     * for the assignment requirements.
+     * @param word The word to add
+     * @return true if the word was added to the dictionary 
+     * (it wasn't already there). */
+    public boolean addWord(String word) {
+    	Objects.requireNonNull(word);
+    	return dict.add(word.toLowerCase());
+    }
+
+
+    /** Return the number of words in the dictionary */
+    public int size()
+    {
+    	
+        return dict.size();
+    }
+
+    /** Is this a word according to this dictionary? */
+    public boolean isWord(String s) {
+    	Objects.requireNonNull(s);
+    	return dict.contains(s.toLowerCase());
+    }
+
+}
---
+++ MOOCTextEditor/src/spelling/DictionaryLL.java
@@ -0,0 +1,50 @@
+package spelling;
+
+import java.util.LinkedList;
+import java.util.Objects;
+
+/**
+ * A class that implements the Dictionary interface using a LinkedList
+ *
+ */
+public class DictionaryLL implements Dictionary 
+{
+
+	private LinkedList<String> dict;
+	
+	public DictionaryLL(){
+		dict = new LinkedList<>();
+	}
+
+
+    /** Add this word to the dictionary.  Convert it to lowercase first
+     * for the assignment requirements.
+     * @param word The word to add
+     * @return true if the word was added to the dictionary 
+     * (it wasn't already there). */
+    public boolean addWord(String word) {
+    	Objects.requireNonNull(word);
+    	String wordToAdd = word.toLowerCase();
+    	if(!dict.contains(wordToAdd)){
+    		dict.add(wordToAdd);
+    		return true;
+    	}
+    	return false;
+    }
+
+
+    /** Return the number of words in the dictionary */
+    public int size()
+    {
+        
+        return dict.size();
+    }
+
+    /** Is this a word according to this dictionary? */
+    public boolean isWord(String s) {
+    	Objects.requireNonNull(s);
+    	return dict.contains(s.toLowerCase());
+    }
+
+    
+}
